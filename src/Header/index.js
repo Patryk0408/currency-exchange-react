18967fc40bc8c11fee72d4878cb56346fe80b479
Patryk Krawczyk <patryk.krawczyk.it@gmail.com>
2023-05-28 21:12:27 +0200

    Added real date and time

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,9 +1,41 @@
+import { useState, useEffect } from "react";
 import "./style.css";
 
-const Header = ({ title }) => (
-    <header className="header">
-        <h1 className="header__title">{title}</h1>
-    </header>
-);
+const Header = ({ title }) => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(()=> {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        },1000)
+    
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+    
+    const showingNowDate = () => {
+        const newDate = new Date();
+    
+        return newDate.toLocaleDateString(
+            undefined,
+            {
+                month: "long",
+                weekday: "long",
+                day: "numeric",
+                year: "numeric"
+            }
+        )
+    }
+
+    return (
+        <header className="header">
+            <p className="header__timer">
+                Dzisiaj jest: {showingNowDate()}, {time.toLocaleTimeString()}
+            </p>
+            <h1 className="header__title">{title}</h1>
+        </header>
+    );
+};
 
 export default Header;
